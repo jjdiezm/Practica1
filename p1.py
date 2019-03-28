7d03ac385e156d8dedcb88d3f1cfa10bbc544e83
QuimDJ <quimdalmases@uoc.edu>
2019-03-28 17:27:26 +0100

    Update p1.py

--- p1.py
+++ p1.py
@@ -8,10 +8,17 @@ Created on Sat Mar 23 19:48:33 2019
 import requests
 from bs4 import BeautifulSoup
 
-url ='https://www.python.org/events/python-events/'
-req = requests.get(url)
-print(req.text[1:200])
+url1="https://www.marca.com/futbol/primera-division/calendario.html?intcmp=MENUMIGA&s_kw=calendario#jornada1"
+url2="http://www.leroymerlin.es/productos/outlet/calefaccion_y_climatizacion_outlet.html"
+req = requests.get(url2)
 
-soup=BeautifulSoup(req.text,"html5lib")
-events = soup.find('ul').findAll('li')
+soup=BeautifulSoup(req.text,"lxml")
+events = soup.find_all("h3")
+
+i=0
+for e in events:
+    if i>=0: 
+        #print("Tot el link: ",e)
+        print("Només text del link: ", e.find("a").contents,"\n")
+        i+=1
 
