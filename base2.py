6ee0363b4c5d9f4e94d77544125794e068dab3fa
QuimDJ <quimdalmases@uoc.edu>
2019-03-29 22:29:48 +0100

    Update base2.py
    Descarrega ja tots els jugadors de tots els equips.
    Genera un dataframe df(Nom, Equip, Funcio, Dorsal)
    
    0  Abelardo Fernández          Alavés          Entrenador          0
    1  Fernando Pacheco             Alavés          Portero                1
    2  Antonio Sivera                   Alavés          Portero              13
    3  Álex Domínguez                Alavés          Portero              31
    4  Carlos Vigaray                   Alavés          Defensa               2

--- base2.py
+++ base2.py
@@ -19,36 +19,39 @@ import pandas as pd
 urls=["https://www.marca.com/futbol/primera-division/calendario.html?intcmp=MENUMIGA&s_kw=calendario",
       "https://www.marca.com/futbol/primera/equipos.html?intcmp=MENUMIGA&s_kw=equipos-y-jugadores"]
 
-equipos_marca=["alaves","athletic","atletico","barcelona","betis","celta","eibar","espanyol","getafe",
+equips_marca=["alaves","athletic","atletico","barcelona","betis","celta","eibar","espanyol","getafe",
                "girona","huesca","leganes","levante","rayo","real-madrid","real-sociedad","sevilla",
                "valencia","valladolid","villarreal"]
 
-url1="https://www.marca.com/futbol/" + equipos_marca[0] + "/plantilla.html?intcmp=MENUMIGA&s_kw=plantilla-y-datos-del-club"
-
-pag_link = url1
-# obtenir codi font de la website
-
-pag_resp = requests.get(pag_link)
-# raspat html
-#pag_content = BeautifulSoup(pag_resp.content, "lxml")
-pag_content = BeautifulSoup(pag_resp.text, "lxml")
-
-# Accés al contingut HTML de la pàgina web.
-
-equipos = pag_content.find_all("ul",class_='plantilla')
-#equipos = pag_content.find_all('ul', {'class': ['plantillas-equipos']}) 
 EQ= pd.DataFrame(columns=('Nom', 'Equip', 'Funcio', 'Dorsal'))
-
-if equipos:
-    element=equipos[0].find_all("li")
-    nomEquip=element[0]['id']
-    entrenador=element[0].find("p",class_="entrenador").text.replace("Entrenador","").strip()
-    EQ=EQ.append({'Nom':entrenador,'Equip': nomEquip, 'Funcio':'Entrenador', 'Dorsal':'0'}, ignore_index=True)
-    dorsals=equipos[0].find_all("strong")
-    noms_jug=equipos[0].find_all("span", class_="dorsal-jugador")
-    pos_jug=equipos[0].find_all("span", class_="posicion")
+#for m in range(len(equips_marca)):
+for m in range(20):
+    url1="https://www.marca.com/futbol/" + equips_marca[m] + "/plantilla.html?intcmp=MENUMIGA&s_kw=plantilla-y-datos-del-club"
+    if m==7:
+        a=1
+    pag_link = url1
+    # obtenir codi font de la website
     
-    for k in range(26):
-        EQ=EQ.append({'Nom':noms_jug[k].text.strip(),'Equip': nomEquip.strip(), 'Funcio':pos_jug[k].text.strip(), 'Dorsal':dorsals[k].text.strip()}, ignore_index=True)  
+    pag_resp = requests.get(pag_link)
+    # raspat html
+    #pag_content = BeautifulSoup(pag_resp.content, "lxml")
+    pag_content = BeautifulSoup(pag_resp.text, "lxml")
     
+    # Accés al contingut HTML de la pàgina web.
+    
+    equipos = pag_content.find_all("ul",class_='plantilla')
+    #equipos = pag_content.find_all('ul', {'class': ['plantillas-equipos']}) 
+    
+    if equipos:
+        element=equipos[0].find_all("li")
+        nomEquip=element[0]['id']
+        entrenador=element[0].find("p",class_="entrenador").text.replace("Entrenador","").strip()
+        EQ=EQ.append({'Nom':entrenador,'Equip': nomEquip, 'Funcio':'Entrenador', 'Dorsal':'0'}, ignore_index=True)
+        dorsals=equipos[0].find_all("strong")
+        noms_jug=equipos[0].find_all("span", class_="dorsal-jugador")
+        pos_jug=equipos[0].find_all("span", class_="posicion")
+        
+        for k in range(len(noms_jug)):
+            EQ=EQ.append({'Nom':noms_jug[k].text.strip(),'Equip': nomEquip.strip(), 'Funcio':pos_jug[k].text.strip(), 'Dorsal':dorsals[k].text.strip()}, ignore_index=True)  
+        
 
