08f4937d0fea68e6e6535a077b389d3bf66359c8
Juanjo Díez <jjdiezm@gmail.com>
2019-04-05 13:26:14 +0200

    Added Facebook of teams

--- ENTREGA/Visualitza_Jugadors.py
+++ ENTREGA/Visualitza_Jugadors.py
@@ -5,42 +5,87 @@ Created on Fri Mar 29 19:26:21 2019
 @author: Grup pràctiques:  Quim Dalmases / Juanjo Díez
 """
 
-#with open('fotos.pickle', 'wb') as fitxer:
-#    pickle.dump(Fotos, fitxer, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-#with open('fotos.pickle', 'wb') as f:
-#    f.write(response)
-
-
 import pickle
-from skimage import io
 import matplotlib.pyplot as plt
 
+with open('fotos.pickle', 'rb') as image_file:
+    img_objects = pickle.load(image_file)
+# tam = len(img_objects.keys())
+# Full teams list
+full_team_list = ['Alavés', 'Athletic', 'Atlético', 'Barça', 'Betis', 'Celta', 'Eibar', 'Espanyol',
+                  'Getafe', 'Girona', 'Huesca', 'Leganés', 'Levante', 'Rayo', 'Real Madrid', 'Real Sociedad',
+                  'Sevilla FC', 'Valencia', 'Valladolid', 'Villarreal']
 
-with open('fotos.pickle', 'rb') as fitxer:
-    c = pickle.load(fitxer)
 
-tam=len(c.keys())
+def team_photos(team_list):
+    """ Function to show the fotos of a team list one by one
 
-def jugadors_equips(l):
-    for e in l:
-        num=0
-        for k in c.keys():
-            if e in k:
-                num+=1
-                plt.imshow(c[k])
-                plt.title(k.replace("_","\n"),fontsize=18)
+    :param team_list: List of teams
+    :return: nothing
+    """
+    for team in team_list:
+        num = 0
+        for key in img_objects.keys():
+            if team in key:
+                num += 1
+                plt.imshow(img_objects[key])
+                plt.title(key.replace("_", "\n"), fontsize=18)
                 plt.show()
-                
-        print("Plantilla "+ str(num) + " jugadors.")
+        plt.show()
+        print("Team {} has {} players registered.".format(str(team), str(num)))
     return
 
 
-# llista total d'equips
-llista_equips=['Alavés','Athletic','Atlético','Barça','Betis','Celta','Eibar','Espanyol',
-               'Getafe','Girona','Huesca','Leganés','Levante','Rayo','Real Madrid','Real Sociedad',
-               'Sevilla FC','Valencia','Valladolid','Villarreal']
+def team_facebook(team_list, cols=5):
+    """ Function to show the fotos of a team list as facebook
 
-# Exemple d'aplicació de la funció.
-jugadors_equips(['Valencia','Valladolid','Villarreal'])
+    :param team_list: List of teams
+    :param cols: number of cols to plot, 5 predefined value if not passed
+    :return: nothing:
+    """
+
+    # For each team in list passed:
+    for team in team_list:
+        # Lists to store team players and names
+        player_list = list()
+        player_names = list()
+        for key in img_objects.keys():
+            if team in key:
+                # store image
+                player_list.append(img_objects[key])
+                # store player name without team name and underscores
+                title = key.replace(team, "")
+                player_names.append(title.replace("_", " "))
+        if len(player_list) > 0:
+            # calculated row number based on number of players and cols set
+            rows = len(player_list)//cols
+            if len(player_list) % cols > 0:
+                rows += 1
+            # array of subplots
+            figure, axes_array = plt.subplots(rows, cols, figsize=(10, 10))
+            # fill array with images
+            for num in range(len(player_list)):
+                i = num//cols
+                j = num % cols
+                axes_array[i, j].imshow(player_list[num])
+                axes_array[i, j].set_title(player_names[num], fontsize=10)
+                axes_array[i, j].axis("off")
+            # Avoid painting axis of void images
+            if len(player_list) % cols > 0:
+                for void_cell in range(cols - (len(player_list) % cols)):
+                    j = cols - void_cell - 1
+                    i = rows - 1
+                    axes_array[i, j].axis("off")
+            # Configure plot by spacing images and adding a title
+            figure.subplots_adjust(hspace=0.3)
+            figure.suptitle(team, fontsize=12, fontweight='bold')
+            plt.show()
+        else:
+            print("No players registered on team: ".format(str(team)))
+    return
+
+
+# Example picking some teams:
+team_facebook(full_team_list[i] for i in [2, 3, 14, 16, 17])
+# Other Example
+# team_photos(full_team_list[18:])
--- WebScraping1.py
+++ WebScraping1.py
@@ -9,38 +9,40 @@ Descripció:
 @author: Grup pràctiques:  Quim Dalmases / Juanjo Díez
 """
 
-# equipos_marca=["alaves","athletic","atletico","barcelona","betis","celta","eibar","espanyol","getafe","girona","huesca","leganes","levante","rayo","real-madrid","real-sociedad","sevilla","valencia","valladolid","villarreal"]
+# equipos_marca = ["alaves", "athletic", "atletico", "barcelona", "betis", "celta", "eibar", "espanyol", "getafe",
+#                  "girona", "huesca", "leganes", "levante", "rayo", "real-madrid", "real-sociedad", "sevilla",
+#                  "valencia", "valladolid", "villarreal"]
 # "https://www.marca.com/futbol/" & equipos_marca[k] & "/plantilla.html?intcmp=MENUMIGA&s_kw=plantilla-y-datos-del-club"
-
 
 # Llibreries per implementar Web scraping.
 import numpy as np
 from bs4 import BeautifulSoup
 import requests
-#import re
+# import re
 import pandas as pd
 import pickle
 from skimage import io
 import time
 
 # URL's on fer webscraping
-urls=["https://www.marca.com/futbol/primera-division/calendario.html?intcmp=MENUMIGA&s_kw=calendario",
-      "https://www.marca.com/futbol/primera/equipos.html?intcmp=MENUMIGA&s_kw=equipos-y-jugadores"]
+urls = ["https://www.marca.com/futbol/primera-division/calendario.html?intcmp=MENUMIGA&s_kw=calendario",
+        "https://www.marca.com/futbol/primera/equipos.html?intcmp=MENUMIGA&s_kw=equipos-y-jugadores"]
 
-equips_marca=["alaves","athletic","atletico","barcelona","betis","celta","eibar","espanyol","getafe",
-               "girona","huesca","leganes","levante","rayo","real-madrid","real-sociedad","sevilla",
-               "valencia","valladolid","villarreal"]
+equips_marca = ["alaves", "athletic", "atletico", "barcelona", "betis", "celta", "eibar", "espanyol", "getafe",
+                "girona", "huesca", "leganes", "levante", "rayo", "real-madrid", "real-sociedad", "sevilla",
+                "valencia", "valladolid", "villarreal"]
 
-agents=["Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)",
-"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
-" Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9a1) Gecko/20070308 Minefield/3.0a1"]
+agents = ["Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)",
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+          "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9a1) Gecko/20070308 Minefield/3.0a1"]
 
-#headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.63 Safari/537.36'}
+# headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
+#                          ' Chrome/49.0.2623.63 Safari/537.36'}
 
- # Implementació de proxys alternatius
- # FONT:  https://www.sslproxies.org/
+# Implementació de proxys alternatius
+# FONT:  https://www.sslproxies.org/
 proxies = ["http://145.253.253.52:8080", "http://34.244.2.233:8123"]
-#pag_resp = requests.get(pag_link, proxies=proxies, timeout=5)  
+# pag_resp = requests.get(pag_link, proxies=proxies, timeout=5)
 
 # Honeypots:
 # Vigilar : Display:None
@@ -48,15 +50,16 @@ proxies = ["http://145.253.253.52:8080", "http://34.244.2.233:8123"]
 
 try:
     # Definim l'estructura del dataframe.
-    EQ= pd.DataFrame(columns=('idDB','Nom', 'Equip', 'Funcio', 'Dorsal'))
+    EQ = pd.DataFrame(columns=('idDB', 'Nom', 'Equip', 'Funcio', 'Dorsal'))
     # Diccinari que guarda les fotografies dels jugadors.
-    Fotos=dict()
+    Fotos = dict()
     # Càlcul del temps de durada del procés de web-scraping.
     time_start = time.time()
     # Bucle d'extracció per cada equip de 1ª divisió.
     for m in range(len(equips_marca)):
         # Construcció de la URL d'accés a la pàgina web de cada equip.
-        pag_link = "https://www.marca.com/futbol/" + equips_marca[m] + "/plantilla.html?intcmp=MENUMIGA&s_kw=plantilla-y-datos-del-club"
+        pag_link = "https://www.marca.com/futbol/" + equips_marca[m] + \
+                   "/plantilla.html?intcmp=MENUMIGA&s_kw=plantilla-y-datos-del-club"
         # obtenir codi font HTML de la website: time_out de 1 segon en cada petició.
         pag_resp = requests.get(pag_link, timeout=1)
         # Si el access al servidor està bloquejat [status_code!=200],executem una petició alternativa
@@ -65,8 +68,8 @@ try:
             print(pag_resp.status_code)
             # fer scraping alternatiu
             # proxy i capçalera diferents. 
-            proxy={'http': proxies[int(np.random.choice(2,1))]}
-            header={'User-Agent': agents[int(np.random.choice(3,1))]}
+            proxy = {'http': proxies[int(np.random.choice(2, 1))]}
+            header = {'User-Agent': agents[int(np.random.choice(3, 1))]}
             # Es torna a provar la petició, ara amb un temps d'espera de 5 segons.
             pag_resp = requests.get(pag_link, proxies=proxy, headers=header, timeout=5)
         # Accés al contingut HTML de la pàgina web.
